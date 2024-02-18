bf262ed1233aa6be7fbe45e43fd1ea188a611a10
ehernandezp <edgardhernandezp@gmail.com>
2024-02-18 09:08:23 -0300

    Generated the project using spring initializer

---
+++ src/main/java/com/dreamseeker/videogamerecommendationservice/VideogameRecommendationServiceProducerApplication.java
@@ -0,0 +1,13 @@
+package com.dreamseeker.videogamerecommendationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogameRecommendationServiceProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogameRecommendationServiceProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamseeker/videogamerecommendationservice/VideogameRecommendationServiceProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamseeker.videogamerecommendationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogameRecommendationServiceProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
