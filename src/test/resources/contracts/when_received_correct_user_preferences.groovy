409c7f38c4a3c3947c0ecd6c6e33a68a005f04c1
ehernandezp <edgardhernandezp@gmail.com>
2024-04-11 08:36:26 -0400

    Added a new contract to test a case with query params

--- src/test/resources/contracts/when_received_correct_user_preferences.groovy
+++ src/test/resources/contracts/when_received_correct_user_preferences.groovy
@@ -34,4 +34,5 @@ Contract.make {
                 ]
         )
     }
+    priority(2)
 }
---
+++ src/test/resources/contracts/when_receiving_exclusions.groovy
@@ -0,0 +1,40 @@
+package contracts
+
+import org.springframework.cloud.contract.spec.Contract
+import org.springframework.cloud.contract.spec.internal.HttpMethods
+
+Contract.make {
+    request {
+        urlPath('/fetch-recommendations') {
+            queryParameters {
+                parameter('exclusions', equalTo('Platformer'))
+            }
+        }
+        method(HttpMethods.PUT)
+        headers {
+            header(contentType(), applicationJson())
+        }
+        body([
+                'userId'         : anyUuid(),
+                'preferredGenres': ['Platformer', 'Stealth', 'Adventure']
+        ]
+        )
+    }
+    response {
+        headers {
+            header(contentType(), applicationJson())
+        }
+        status(200)
+        body(
+                [
+                        'videogames': [
+                                [
+                                        'name' : 'Red Dead Redemption',
+                                        'genre': 'Adventure'
+                                ]
+                        ]
+                ]
+        )
+    }
+    priority(1)
+}
